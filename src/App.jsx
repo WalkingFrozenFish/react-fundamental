762c4dc3a6945cf6c0602bee1161dcbd85cf6e95
WalkingFrozenFish <chachin.ol@yandex.kz>
2022-01-23 02:22:58 +0600

    Конспект

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 import ClassCounter from './components/ClassCounter.jsx';
 import Counter from './components/Counter.jsx';
 import Example from './components/Example.jsx';
+import PostForm from './components/PostForm.jsx';
 import PostItem from './components/PostItem.jsx';
 import PostList from './components/PostList.jsx';
 import MyButton from './components/UI/button/MyButton.jsx';
@@ -15,35 +16,18 @@ const App = () => {
     { id: 3, title: "Java", body: "Description" },
   ]);
 
-  const [post, setPost] = useState({
-    title: "",
-    body: "",
-  });
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
 
-  const addNewPost = (e) => {
-    e.preventDefault();
-    setPosts([...posts, { ...post, id: Date.now() }]);
-    setPost({ title: "", body: "" })
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
   }
 
   return (
     <div className='App'>
-      <form>
-        <MyInput
-          value={post.title}
-          onChange={e => setPost({ ...post, title: e.target.value })}
-          type="text"
-          placeholder='Название поста'
-        />
-        <MyInput
-          value={post.body}
-          onChange={e => setPost({ ...post, body: e.target.value })}
-          type="text"
-          placeholder='Описание поста'
-        />
-        <MyButton onClick={addNewPost}>Создать пост</MyButton>
-      </form>
-      <PostList posts={posts} title={"Языки программирования"} />
+      <PostForm create={createPost} />
+      <PostList remove={removePost} posts={posts} title={"Языки программирования"} />
     </div>
   );
 };
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import PostItem from './PostItem';
 
 const PostList = (props) => {
+    console.log(props)
     return (
         <div>
             <h1 style={{ textAlign: 'center', color: 'green', }}>
                 {props.title}
             </h1>
             {props.posts.map((post, index) => {
-                return <PostItem number={index + 1} post={post} key={post.id} />
+                return <PostItem remove={props.remove} number={index + 1} post={post} key={post.id} />
             })}
         </div>
     );
